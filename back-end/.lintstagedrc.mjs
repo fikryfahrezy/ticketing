5777b8b0616c1829bbb5c1316700a9ad474f265b
Fikry Fahrezy <55774934+fikryfahrezy@users.noreply.github.com>
2026-03-08 14:24:14 +0700

    Migrate from `Husky` to `lefthook`

--- back-end/.lintstagedrc.mjs
+++
@@ -1,8 +0,0 @@
-/**
- * @filename: .lintstagedrc.mjs
- * @type {import('lint-staged').Configuration}
- */
-export default {
-  "*.{md,json,yaml}": ["prettier --write"],
-  "*.{html,css,tsx,jsx}": ["eslint", "prettier --write"]
-}
